4b366840f7d45ae68aebfee22acddc95ba8f6247
Yunus Oksuz <yunusoksuz@gmail.com>
2018-01-01 21:18:18 +0300

    colorful geojson output

--- src/main/java/com/openvehicletracking/device/xtakip/XTakip.java
+++ src/main/java/com/openvehicletracking/device/xtakip/XTakip.java
@@ -141,33 +141,98 @@ public class XTakip implements Device {
 
     @Override
     public GeoJsonResponse responseAsGeoJson(List<? extends LocationMessage> messages) {
+        Objects.requireNonNull(messages, "messages cannot be null");
+        final GeoJsonResponse geoJsonResponse = new GeoJsonResponse();
+
         LineStringGeometry lineStringGeometry = new LineStringGeometry();
-        PointGeometry endPoint = new PointGeometry();
-        PointGeometry startPoint = new PointGeometry();
-        LocationMessage firstMessage = messages.get(messages.size() - 1);
-        LocationMessage lastMessage = messages.get(0);
+        PointGeometry marker;
+        Boolean ignKeyOff = null;
+        String startColor = getRgbColor();
+        String endColor = startColor;
+        int startId = 1;
+        int endId = startId;
+        for (LocationMessage message : messages) {
+            LProtocolMessage m = (LProtocolMessage) message;
 
-        List<GeoJsonProperty> properties = new ArrayList<>();
+            if (m.getDeviceState().getOfflineRecord() == Boolean.TRUE) {
+                continue;
+            }
 
-        messages.forEach(m -> lineStringGeometry.addPoint(new Point(m.getLatitude(), m.getLongitude())));
+            Boolean currIgnKeyOff = m.getDeviceState().getIgnitiKeyOff();
+            // ignition key status changed
+            if (!Objects.equals(ignKeyOff, currIgnKeyOff)) {
+                // create marker for ignition status change location
+                marker = new PointGeometry();
+                marker.addPoint(createPoint(m));
 
-        startPoint.addProperty(new GeoJsonProperty("start-point.png", "markerIcon"));
-        startPoint.addPoint(new Point(firstMessage.getLatitude(), firstMessage.getLongitude()));
+                // if ignition key on, that means vehicle starting to move
+                if (currIgnKeyOff == Boolean.FALSE) {
+                    // add marker
+                    geoJsonResponse.addFeature(createFeatureForPoint(startId, startColor, marker, false));
 
-        endPoint.addPoint(new Point(lastMessage.getLatitude(), lastMessage.getLongitude()));
-        endPoint.addProperty(new GeoJsonProperty("end-point.png", "markerIcon"));
+                    // configure end color, and end id
+                    endColor = startColor;
+                    endId = startId;
+
+                    // create new color and id for new start
+                    startColor = getRgbColor();
+                    startId++;
+                } else if (currIgnKeyOff == Boolean.TRUE) {
+                    // vehicle stopped use configured endId and end color here
+                    geoJsonResponse.addFeature(createFeatureForPoint(endId, endColor, marker, true));
+                }
+
+                if (lineStringGeometry.getCoordinates().size() > 1) {
+                    // put collected lines as feature with end color and endId
+                    geoJsonResponse.addFeature(createFeature(endId, endColor, lineStringGeometry));
+                    // create new lineString for new start
+                    lineStringGeometry = new LineStringGeometry();
+                }
+
+            }
+
+            lineStringGeometry.addPoint(createPoint(m));
+            ignKeyOff = currIgnKeyOff;
+        }
 
 
-        GeoJsonResponse geoJsonResponse = new GeoJsonResponse();
-        geoJsonResponse.addFeature(new Feature(properties, lineStringGeometry));
-        geoJsonResponse.addFeature(new Feature(null, endPoint));
-        geoJsonResponse.addFeature(new Feature(null, startPoint));
+
         return geoJsonResponse;
+    }
+
+    private Feature createFeatureForPoint(int id, String color, PointGeometry geo, boolean ignKeyOff) {
+        List<GeoJsonProperty> properties = new ArrayList<>();
+        properties.add(new GeoJsonProperty("marker-color", color));
+        properties.add(new GeoJsonProperty("marker-size", "small"));
+        properties.add(new GeoJsonProperty("marker-symbol", ignKeyOff ? "parking" : "rocket"));
+        properties.add(new GeoJsonProperty("id", String.valueOf(id)));
+        return new Feature(id, properties, geo);
+    }
+
+    private Feature createFeature(int id, String color, Geometry geometry) {
+        List<GeoJsonProperty> properties = new ArrayList<>();
+        properties.add(new GeoJsonProperty("stroke", color));
+        properties.add(new GeoJsonProperty("id", String.valueOf(id)));
+        return new Feature(id, properties, geometry);
     }
 
     @Override
     public ResponseAdapter getResponseAdapter() {
         return null;
+    }
+
+
+    private Point createPoint(LProtocolMessage message) {
+        return new Point(message.getLatitude(), message.getLongitude());
+    }
+
+
+    private String getRgbColor() {
+        Random randomGenerator = new Random();
+        int red = randomGenerator.nextInt(256);
+        int green = randomGenerator.nextInt(256);
+        int blue = randomGenerator.nextInt(256);
+        return String.format("#%02x%02x%02x", red, green, blue);
     }
 
 }
